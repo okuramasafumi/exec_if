ed56be5d35eab2b3d29f1642d445fa341b5afd10
OKURA Masafumi <masafumi.o1988@gmail.com>
2019-12-16 21:11:23 +0900

    Rails-ish evaluation of condition

--- lib/exec_if.rb
+++ lib/exec_if.rb
@@ -2,9 +2,16 @@ require "exec_if/version"
 
 class Object
   def exec_if(obj, &blk)
-    if obj
-      blk.call(self, obj)
-    else
+    case obj
+    when Proc
+      obj.call(self) ? blk.call(self, obj) : self
+    when Symbol
+      public_send(obj) ? blk.call(self, obj) : self
+    when String
+      eval(obj) ? blk.call(self, obj) : self
+    when Object
+      obj ? blk.call(self, obj) : self
+    when nil, false
       self
     end
   end
--- test/exec_if_test.rb
+++ test/exec_if_test.rb
@@ -16,21 +16,65 @@ class ExecIfTest < Minitest::Test
     end
   end
 
-  def test_it_executes_block_if_argument_is_truthy
-    assert_output "foo" do
-      @object.exec_if(true) { print 'foo' }
+  def test_it_does_not_execute_block_and_returns_self_if_argument_is_nil
+    assert_output("") do
+      @result = @object.exec_if(nil) { puts 'This should never be put' }
     end
+    assert_equal @result, @object
+  end
+
+  def test_it_does_not_execute_block_and_returns_self_if_argument_is_false
+    assert_output "" do
+      @return = @object.exec_if(false) { print 'This should never be put' }
+    end
+    assert_equal @object, @return
+  end
+
+  def test_it_executes_block_if_argument_is_a_proc_and_result_of_calling_is_truthy
+    assert_output "foo" do
+      @result = @object.exec_if(Proc.new {|obj| obj.is_a?(String) }) { print 'foo'; 42 }
+    end
+    assert_equal 42, @result
+  end
+
+  def test_it_does_not_execute_block_and_return_self_if_argument_is_a_proc_and_result_of_calling_is_falsy
+    assert_output "" do
+      @return = @object.exec_if(Proc.new {|obj| obj.is_a?(Integer) }) { print 'foo' }
+    end
+    assert_equal @object, @return
+  end
+
+  def test_it_executes_block_if_argument_is_a_symbol_and_result_of_method_named_as_symbol_is_truthy
+    assert_output "foo" do
+      @return = @object.exec_if(:ascii_only?) { print 'foo'; 'foo' }
+    end
+    assert_equal 'foo', @return
+  end
+
+  def test_it_does_not_execute_block_and_return_self_if_argument_is_symbol_and_result_of_method_named_as_symbol_is_falsy
+    assert_output "" do
+      @return = @object.exec_if(:empty?) { print 'foo' }
+    end
+    assert_equal @object, @return
+  end
+
+  def test_it_executes_block_if_argument_is_a_string_and_result_of_eval_is_truthy
+    assert_output "foo" do
+      @return = @object.exec_if('42.is_a?(Integer)') { print 'foo'; 'foo' }
+    end
+    assert_equal 'foo', @return
+  end
+
+  def test_it_does_not_execute_block_and_returns_self_if_argument_is_a_string_and_result_of_eval_is_falsy
+    assert_output "" do
+      @return = @object.exec_if('42.is_a?(String)') { print 'foo'; 'foo' }
+    end
+    assert_equal @object, @return
   end
 
   def test_it_executes_block_with_self_and_argument
     assert_output "foobar: 1" do
       @object.exec_if(1) {|obj, i| print "#{obj}: #{i}"}
-    end
-  end
-
-  def test_it_returns_self_if_argument_is_falsy
-    assert_output "" do
-      @object.exec_if(false) { print 'This should never be put' }
     end
   end
 end

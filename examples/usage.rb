7397b03919492400a66abe791b32ef9229c46d31
OKURA Masafumi <masafumi.o1988@gmail.com>
2019-12-20 21:44:06 +0900

    Improve usage example

--- examples/usage.rb
+++ examples/usage.rb
@@ -2,6 +2,6 @@ require_relative '../lib/exec_if'
 
 puts 'hello'.concat(' world!').
   exec_if(:ascii_only?) { upcase }.
-  exec_if("end_with?('?')") { delete_suffix('?') }.
+  exec_if("end_with?('?')") { sub('!', '?') }. # not executed
   exec_if(Proc.new {|str| str.length >= 5 }) { delete_prefix('HEL') }.
   == 'LO WORLD!'
